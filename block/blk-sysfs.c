daed8eeecd031b58475b68be2c48eb35033a671d
Adam W. Willis <return.of.octobot@gmail.com>
2019-11-28 13:45:53 +0100

    mm: apply init protection
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Change-Id: I12ea7da6a465500852102b5b6eff3bec34a948c5
    [TogoFire]: adapt to k5.4.
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -102,6 +102,9 @@ queue_ra_store(struct request_queue *q, const char *page, size_t count)
 	if (ret < 0)
 		return ret;
 
+	if (!strcmp(current->comm, "init"))
+		ra_kb = SZ_128K;
+
 	q->backing_dev_info->ra_pages = ra_kb >> (PAGE_SHIFT - 10);
 
 	return ret;
