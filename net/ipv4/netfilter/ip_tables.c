efeaaa2fdbc68e8dc64d61f8776fc395ef652fd5
Oktapra Amtono <oktapra.amtono@gmail.com>
2020-04-20 20:49:21 +0700

    net: netfilter: Disable nf_log_trace
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Change-Id: Ic01288679bda3f8ed9a3a4ba77cb8043e062d172
    [ Tashar02: Compile out tracing using CONFIG_DEBUG_KERNEL macro ]
    Co-authored-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    [TogoFire]: adapt to k5.4.
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- net/ipv4/netfilter/ip_tables.c
+++ net/ipv4/netfilter/ip_tables.c
@@ -143,6 +143,7 @@ static const char *const comments[] = {
 	[NF_IP_TRACE_COMMENT_POLICY]	= "policy",
 };
 
+#ifdef CONFIG_DEBUG_KERNEL
 static const struct nf_loginfo trace_loginfo = {
 	.type = NF_LOG_TYPE_LOG,
 	.u = {
@@ -152,6 +153,7 @@ static const struct nf_loginfo trace_loginfo = {
 		},
 	},
 };
+#endif
 
 /* Mildly perf critical (only if packet tracing is on) */
 static inline int
@@ -208,9 +210,11 @@ static void trace_packet(struct net *net,
 		    &chainname, &comment, &rulenum) != 0)
 			break;
 
+#ifdef CONFIG_DEBUG_KERNEL
 	nf_log_trace(net, AF_INET, hook, skb, in, out, &trace_loginfo,
 		     "TRACE: %s:%s:%s:%u ",
 		     tablename, chainname, comment, rulenum);
+#endif
 }
 #endif
 
--- net/ipv6/netfilter/ip6_tables.c
+++ net/ipv6/netfilter/ip6_tables.c
@@ -168,6 +168,7 @@ static const char *const comments[] = {
 	[NF_IP6_TRACE_COMMENT_POLICY]	= "policy",
 };
 
+#ifdef CONFIG_DEBUG_KERNEL
 static const struct nf_loginfo trace_loginfo = {
 	.type = NF_LOG_TYPE_LOG,
 	.u = {
@@ -177,6 +178,7 @@ static const struct nf_loginfo trace_loginfo = {
 		},
 	},
 };
+#endif
 
 /* Mildly perf critical (only if packet tracing is on) */
 static inline int
@@ -233,9 +235,11 @@ static void trace_packet(struct net *net,
 		    &chainname, &comment, &rulenum) != 0)
 			break;
 
+#ifdef CONFIG_DEBUG_KERNEL
 	nf_log_trace(net, AF_INET6, hook, skb, in, out, &trace_loginfo,
 		     "TRACE: %s:%s:%s:%u ",
 		     tablename, chainname, comment, rulenum);
+#endif
 }
 #endif
 
