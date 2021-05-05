cf942b6540cc7b1e8ae8489c32d77f2bc256bd5b
David Collins <collinsd@codeaurora.org>
2021-05-04 17:48:31 -0700

    input: qpnp-power-on: correct header stub function definitions
    Update the definition of all stub functions to include
    "static inline".  This avoids an unused function error if a
    driver includes the header when CONFIG_INPUT_QPNP_POWER_ON
    is not enabled.
    
    Signed-off-by: David Collins <collinsd@codeaurora.org>
    Change-Id: I4008edb61caa7cb103afd87205929f5d9df12fda
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    (cherry picked from commit 825ca76689bc38c30bfd6cdf78b74d9961bee8f5)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- include/linux/input/qpnp-power-on.h
+++ include/linux/input/qpnp-power-on.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2012-2015, 2017-2019, The Linux Foundation.
+ * Copyright (c) 2012-2015, 2017-2019, 2021 The Linux Foundation.
  * All rights reserved.
  */
 
@@ -73,7 +73,7 @@ int qpnp_pon_modem_pwr_off(enum pon_power_off_type type);
 
 #else
 
-static int qpnp_pon_system_pwr_off(enum pon_power_off_type type)
+static inline int qpnp_pon_system_pwr_off(enum pon_power_off_type type)
 {
 	return -ENODEV;
 }
@@ -89,7 +89,7 @@ static inline int qpnp_pon_trigger_config(enum pon_trigger_source pon_src,
 	return -ENODEV;
 }
 
-int qpnp_pon_wd_config(bool enable)
+static inline int qpnp_pon_wd_config(bool enable)
 {
 	return -ENODEV;
 }
