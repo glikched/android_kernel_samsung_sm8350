a79dc486521f578bd0d2442fc3df7d4b153dd845
TogoFire <italomellopereira@gmail.com>
2021-09-04 15:53:20 -0300

    drivers: zram: set lzo only when it is called
    Signed-off-by: TogoFire <italomellopereira@gmail.com>
    Change-Id: Ia24c83d362336af9ebdaa05624d793ebdcbe5789

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -15,8 +15,6 @@
 #include "zcomp.h"
 
 static const char * const backends[] = {
-	"lzo",
-	"lzo-rle",
 #if IS_ENABLED(CONFIG_CRYPTO_LZ4)
 	"lz4",
 #endif
@@ -28,6 +26,10 @@ static const char * const backends[] = {
 #endif
 #if IS_ENABLED(CONFIG_CRYPTO_ZSTD)
 	"zstd",
+#endif
+#if IS_ENABLED(CONFIG_CRYPTO_LZO)
+	"lzo",
+	"lzo-rle",
 #endif
 	NULL
 };
