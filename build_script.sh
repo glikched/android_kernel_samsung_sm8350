52708ed1a010bdb09cde811022476f1215fba112
glikched <florianvictorcristea@gmail.com>
2024-02-03 19:02:54 +0200

    build_script: rework

--- build_script.sh
+++ build_script.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-export ARCH=arm64
-mkdir out
+SRC_DIR=$(pwd)
+TC_DIR=/home/victor/toolchains/clang-r383902b1
+JOBS=64
+MAKE_PARAMS="-j$JOBS -C $SRC_DIR O=$SRC_DIR/out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- LLVM=1 CROSS_COMPILE=$TC_DIR/bin/llvm-"
+export PATH="$TC_DIR/bin:$PATH"
+make $MAKE_PARAMS vendor/r9q_eur_openx_defconfig
+make $MAKE_PARAMS
+make $MAKE_PARAMS INSTALL_MOD_PATH=modules INSTALL_MOD_STRIP=1 modules_install
 
-BUILD_CROSS_COMPILE=~/toolchain/gcc/bin/aarch64-linux-android-
-KERNEL_LLVM_BIN=~/toolchain/llvm-arm-toolchain-ship-10.0/bin/clang
-CLANG_TRIPLE=aarch64-linux-gnu-
-KERNEL_MAKE_ENV=""
-BUILD_MODULES_DIR="$(pwd)/out/modules"
-MOD_DIR="$ZIP_DIR"/modules/vendor/lib/modules
-K_MOD_DIR="$(pwd)/out/modules"
-
-make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CONFIG_SECTION_MISMATCH_WARN_ONLY=y vendor/r9q_eur_openx_defconfig
-make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CONFIG_SECTION_MISMATCH_WARN_ONLY=y
-make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CONFIG_SECTION_MISMATCH_WARN_ONLY=y INSTALL_MOD_PATH=$BUILD_MODULES_DIR INSTALL_MOD_STRIP=1 modules_install
-
+rm -rf modules
 mkdir modules
-find "$(pwd)/out/modules" -type f -iname "*.ko" -exec cp -r {} ./modules/ \;
-cp ./out/arch/arm64/boot/Image ./Image
-cp ./out/arch/arm64/boot/dtbo.img ./dtbo.img
+find "$(pwd)/out/modules" -type f -iname "*.ko" -exec cp -r {} ./AnyKernel3/modules/vendor/lib/modules/ \;
 
+cp ./out/arch/arm64/boot/Image ./AnyKernel3/
+cd AnyKernel3
+zip -r9 AQUA_Kernel_KSU-v1.0.zip . -x '*.git*' '*patch*' '*ramdisk*' 'LICENSE' 'README.md'
+cd ..
