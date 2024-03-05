abaadfe6daae46b8d8d9e31f74ec95c83a537ba9
glikched <florianvictorcristea@gmail.com>
2024-03-05 12:52:46 +0200

    aqua: move to Neutron Clang

--- build_script.sh
+++ build_script.sh
@@ -8,8 +8,8 @@ MOD_DIR="$ZIP_DIR/modules/vendor/lib/modules"
 K_MOD_DIR="$(pwd)/out/modules"
 
 # Enviorment Variables
-SRC_DIR=$(pwd)
-TC_DIR=$(pwd)/clang
+SRC_DIR="$(pwd)"
+TC_DIR="$HOME/toolchains/neutron-clang"
 JOBS="$(nproc --all)"
 MAKE_PARAMS="-j$JOBS -C $SRC_DIR O=$SRC_DIR/out ARCH=arm64 CC=clang CLANG_TRIPLE=$TC_DIR/bin/aarch64-linux-gnu- LLVM=1 CROSS_COMPILE=$TC_DIR/bin/llvm-"
 export PATH="$TC_DIR/bin:$PATH"
@@ -30,6 +30,16 @@ if [[ "$*" == *"--ksu"* ]]; then
     KSU="true"
 else
     KSU="false"
+fi
+
+if [ -d "$TC_DIR" ]; then
+    echo "Neutron Clang is already there"
+    echo "Credits to dakkshesh07"
+else
+    echo "Fetching Neutron Clang with antman script"
+    echo "Credits to dakkshesh07"
+    mkdir -p "$HOME/toolchains/neutron-clang"; cd "$HOME/toolchains/neutron-clang"; curl -LO "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman"; chmod +x antman; ./antman -S
+    cd $SRC_DIR
 fi
 
 # Check the value of KSU
